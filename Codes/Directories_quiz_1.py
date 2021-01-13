0f87766d75e46caee6e2eec91bd62901f37de503
GergelyOlah <olah.gergely.imre@gmail.com>
2021-01-13 07:23:09 +0000

    CSV added

--- Codes/Directories_quiz_1.py
+++ Codes/Directories_quiz_1.py
@@ -2,8 +2,8 @@ def create_python_script(filename):
   comments = "# Start of a new Python program"
   with open (filename, "w") as script:
     import os
-    filesize = os.path.getsize(filename)
     script.write(comments)
+  filesize = os.path.getsize(filename)
   return filesize
 
 print(create_python_script("program.py"))
