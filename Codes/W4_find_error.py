af9a5fdb400cf7c1230241f44b89489ad193376e
GergelyOlah <olah.gergely.imre@gmail.com>
2021-01-15 14:26:47 +0000

    W4 added

--- Codes/W4_find_error.py
+++ Codes/W4_find_error.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-
 import sys
 import os
 import re
 
+
 def error_search(log_file):
     error = input("What is the error? ")
     returned_errors = []
-    with open (log_file, mode = "r", encoding="UTF8") as file:
-        for log in log_file.readlines():
+    with open (log_file, mode = "r", encoding="UTF-8") as file:
+        for log in file.readlines():
             error_patterns = ["error"]
             for i in range(len(error.split(" "))):
                 error_patterns.append(r"{}".format(error.split(" ")[i].lower))
@@ -17,12 +17,12 @@ def error_search(log_file):
         file.close()
     return returned_errors
 
+
 def file_output(returned_errors):
     with open(os.path.expanduser("~") + "/data/errors_found.log", "w") as file:
         for error in returned_errors:
             file.write(error)
             file.close()
-
 if __name__ == "__main__":
     log_file = sys.argv[1]
     returned_errors = error_search(log_file)
