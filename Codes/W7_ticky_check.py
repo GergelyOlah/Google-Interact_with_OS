ac1a697f0070670def8d95e6972a738f94c43733
GergelyOlah <olah.gergely.imre@gmail.com>
2021-01-16 15:48:26 +0000

    W7 solved

--- Codes/W7_ticky_check.py
+++ Codes/W7_ticky_check.py
@@ -3,35 +3,38 @@
 import re
 import sys
 
-with open (sys.argv[1], r) as logfile:
+#with open (sys.argv[1], r) as logfile:
+with open ("W7_logfile.txt", "r") as logfile:
     errors = {}
     users = {}
     for line in logfile.readlines():
-        error = re.search(r"ERROR ([\w]*) \((\w*)\(", line)
-        info = re.search(r"INFO ([\w]*) \[.*\] \((\w*)\(", line)
-
+        error = re.search(r"ERROR: ([\w ]*) .* \((\w*)\)", line)
+        info = re.search(r"INFO: ([\w ]*) .* \((\w*)\)", line)
+        print(error)
+        print(error[1])
+        print(error[2])
         if info:
-            if users[info[2]] not in users:
+            if info[2] not in users:
                 users[info[2]] = []
             users[info[2]][0] += users.get(info[2][0], 0)
         elif error:
-            if users[info[2]] not in users:
-                users[info[2]] = []
-            errors[error] += errors.get(error[1], 0)
-            users[info[2]][1] += users.get(info[2][1], 0)
+            if error[2] not in users:
+                users[error[2]] = []
+          #  errors[error[1]] += errors.get(error[1], 0)
+            users[error[2]][1] += users.get(error[2][1], 0)
         else:
             continue
 
-    errors.sort(key = lambda x: x[1])
-    users.sort(key = lambda x: x[0])
+    errors_sorted = sorted(errors, key = lambda x: x[1])
+    users_sorted = sorted(users, key = lambda x: x[0])
     logfile.close()
 
 with open("error_message.csv", "w") as error_file:
-    for line in errors:
+    for line in errors_sorted:
         error_file.write(line)
     error_file.close()
 
 with open("user_statistics", "w") as user_file:
-    for line in users:
+    for line in users_sorted:
         user_file.write(line)
     user_file.close()
